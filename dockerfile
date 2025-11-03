5f92efc4ffc069726203df882e0d6c15ed72a60a
Francisco <facuna490@gmail.com>
2025-11-03 15:05:08 -0600

    login creado exitosamente

--- dockerfile
+++ dockerfile
@@ -1,3 +1,14 @@
 FROM php:8.3-apache
+
+# Instalar dependencias necesarias
+RUN apt-get update && apt-get install -y \
+        libzip-dev \
+        libonig-dev \
+        libxml2-dev \
+        default-mysql-client \
+        && docker-php-ext-install pdo pdo_mysql mysqli
+
+# Copiar código al contenedor
 COPY src/ /var/www/html/
+
 EXPOSE 80
---
+++ src/config/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'db'; 
+$dbname = 'tareas_db';
+$username = 'root';
+$password = '1234';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("❌ Error de conexión: " . $e->getMessage());
+}
+?>
