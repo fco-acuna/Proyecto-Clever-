63facc05d71da5b6ef86bc025a3ed24818e6feaf
Francisco <facuna490@gmail.com>
2025-11-10 17:26:58 -0600

    vista boards lista

---
+++ src/views/board.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Board</title>
+    <link rel="stylesheet" href="../CSS/board.css">
+    
+</head>
+<body>
+    <div class="container_dashboards">
+        <div class="container_titulo">
+            <h1>Dashboards</h1>
+            <a href="">New Dashboard</a>
+        </div>
+
+        <div class="dashboard">
+            <div class="titulo_dashboard">
+                <h2>Marketing Dashboard</h2>
+                <a href="">Ver Dashboard</a>
+            </div>
+            <div class="tasks">
+                <div class="completed">
+                    <p>Completadas</p>
+                    <p>32 tareas</p>
+                </div>
+                <div class="progress-bar">
+                    <div class="progress completed"></div>
+                </div>
+                <div class="in_progress">
+                    <p>Trabajando</p>
+                    <p>15 tareas</p>
+                </div>
+                <div class="progress-bar">
+                    <div class="progress in-progress "></div>
+                </div>
+                <div class="backlog">
+                    <p>Pendientes</p>
+                    <p>26 tareas</p>
+                </div>
+                <div class="progress-bar">
+                    <div class="progress backlog"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="dashboard">
+            <div class="titulo_dashboard">
+                <h2>Accounting Dashboard</h2>
+                <a href="">Ver Dashboard</a>
+            </div>
+            <div class="tasks">
+                <div class="completed">
+                    <p>Completadas</p>
+                    <p>12 tareas</p>
+                </div>
+                <div class="progress-bar">
+                    <div class="progress completed"></div>
+                </div>
+                <div class="in_progress">
+                    <p>Trabajando</p>
+                    <p>5 tareas</p>
+                </div>
+                <div class="progress-bar">
+                    <div class="progress in-progress "></div>
+                </div>
+                <div class="backlog">
+                    <p>Pendientes</p>
+                    <p>4 tareas</p>
+                </div>
+                <div class="progress-bar">
+                    <div class="progress backlog"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
